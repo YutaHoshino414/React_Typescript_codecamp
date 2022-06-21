2f660da3ee6b541254c071cc09b5eb7a660a72b9
USER <y.hoshino25@gmail.com>
2022-06-21 22:51:27 +0900

    Add base styles for input-field

--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -1,9 +1,11 @@
 import React from "react";
+import './styles.css';
 
 const InputField = () =>{
     return (
         <form action="" className="input">
-
+            <input className="input__box" type="text" placeholder="Enter a task" />
+            <button className="input_submit" type="submit" >Go</button>
         </form>
     )
 }
