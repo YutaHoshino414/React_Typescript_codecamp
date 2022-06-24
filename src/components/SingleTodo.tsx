2786053617dc316fc7fce5ff39f581f86e30c47c
USER <y.hoshino25@gmail.com>
2022-06-24 10:29:24 +0900

    Add task-complete(isDone) funcitionality on singleTodo comp

--- src/components/SingleTodo.tsx
+++ src/components/SingleTodo.tsx
@@ -1,6 +1,7 @@
 import { Todo } from "../model";
 import { AiFillEdit, AiFillDelete } from "react-icons/ai";
 import { MdDone } from "react-icons/md";
+import TodoList from "./TodoList";
 
 type Props = {
   todo: Todo,
@@ -8,13 +9,27 @@ type Props = {
   setTodos: React.Dispatch<React.SetStateAction<Todo[]>>
 }
 const SingleTodo = ({ todo, todos, setTodos } :Props) => {
+
+  const handleDone = (id: number) =>{
+    // check isDone: boolean
+    setTodos(todos.map((todo) => todo.id === id ? {
+        ...todo, isDone: !todo.isDone
+    }: todo ))
+  }
+
   return (
       <form className="todo__single">
+        {todo.isDone ? (
+          <s className="todo__single--text">{todo.todo}</s>
+        ):(
           <span className="todo__single--text">{todo.todo}</span>
+        )}
           <div>
             <span className="icon"><AiFillEdit /></span>
             <span className="icon"><AiFillDelete /></span>
-            <span className="icon"><MdDone /></span>
+            <span className="icon"
+              onClick={()=>handleDone(todo.id)}
+            ><MdDone /></span>
           </div>
 
       </form>
